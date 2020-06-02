8e5547610e84cc85fa4c2acf7c997ba77ef42569
guzhongren <guzhongren@live.cn>
2020-06-02 21:31:36 +0800

    feat(spyOn): spyOn

--- test/attribute3.test.ts
+++ test/attribute3.test.ts
@@ -8,14 +8,18 @@ describe('attribute', () => {
   afterEach(() =>{
     windowSpy.mockRestore()
   })
-  // it('jest.spyOn', () => {
-  //   const newURL = "http://test.com"
-  //   windowSpy.mockImplementation(() => {
-  //     location: {
-  //       href: ''
-  //     }
-  //   })
-  //   changeHref(newURL)
-  //   expect(windowSpy.location.href).toBe(newURL)
-  // })
+
+  it.todo("jest.spyOn 不适合 spy 属性， 一般用于方法")
+  it('jest.spyOn', () => {
+    expect(window.location.href).toBe('http://localhost/')
+    const newURL = "http://test.com"
+    windowSpy.mockImplementation(() => ({
+      location: {
+        href: ''
+      }
+    }))
+    changeHref(newURL)
+    console.log(window.location.href)
+    expect(window.location.href).toEqual('')
+  })
 })
